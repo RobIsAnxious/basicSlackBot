7bd21ebb0f6111dcf5db9889969f86c5e2dac762
Rob Moore <rmoore.pageonepower@gmail.com>
2015-09-28 15:55:47 -0600

    changed webhook path

--- app.js
+++ app.js
@@ -6,26 +6,21 @@ var dicebot = require('./dicebot');
 var app = express();
 var port = process.env.PORT || 3000;
 
-
+// body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// testing purposes only
-app.get('/', function (req, res) { res.status(200).send('test') });
+// test route
+app.get('/', function (req, res) { res.status(200).send("Hello world!") });
 
-// hellobot
-app.post('/hello', hellobot);
-
-// dicebot
-app.post('/roll', dicebot);
-
-
-// basic error handler
+// error handler
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
 });
 
-
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
 });
+
+app.post('/hello', hellobot);
+app.post('/roll', dicebot);
--- dicebot.js
+++ dicebot.js
@@ -1,67 +1,13 @@
 var request = require('request');
 
-module.exports = function (req, res, next) {
-  // default roll is 2d6
-  var matches;
-  var times = 2;
-  var die = 6;
-  var rolls = [];
-  var total = 0;
-  var botPayload = {};
 
-  if (req.body.text) {
-    // parse roll type if specified
-    matches = req.body.text.match(/^(\d{1,2})d(\d{1,2})$/);
-
-    if (matches && matches[1] && matches[2]) {
-      times = matches[1];
-      die = matches[2];
-
-    } else {
-      // send error message back to user if input is bad
-      return res.status(200).send('<number>d<sides>');
-    }
-  }
-
-  // roll dice and sum
-  for (var i = 0; i < times; i++) {
-    var currentRoll = roll(1, die);
-    rolls.push(currentRoll);
-    total += currentRoll;
-  }
-
-  // write response message and add to payload
-  botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' +
-                    rolls.join(' + ') + ' = *' + total + '*';
-
-  botPayload.username = 'dicebot';
-  botPayload.channel = req.body.channel_id;
-  botPayload.icon_emoji = ':game_die:';
-
-  // send dice roll
-  send(botPayload, function (error, status, body) {
-    if (error) {
-      return next(error);
-
-    } else if (status !== 200) {
-      // inform user that our Incoming WebHook failed
-      return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-
-    } else {
-      return res.status(200).end();
-    }
-  });
-}
-
-
-function roll (min, max) {
+function roll(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
 function send (payload, callback) {
   var path = process.env.INCOMING_WEBHOOK_PATH;
-  var uri = 'https://hooks.slack.com/services' + path;
+  var uri = 'https://hooks.slack.com/services/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';
 
   request({
     uri: uri,
@@ -74,4 +20,45 @@ function send (payload, callback) {
 
     callback(null, response.statusCode, body);
   });
+}
+
+module.exports = function(req, res, next) {
+  var matches;
+  var times = 2;
+  var die = 6;
+  var rolls = [];
+  var total = 0;
+  var botPayload = {};
+
+  if (req.body.text){
+    //parse roll type if specified
+    matches = req.body.text.match(/^(\d{1,2})d(\d{1,2})$/)
+
+    if (matches && matches[1] && matches[2]){
+      times = matches[1];
+      die = matches[2];
+    } else {
+      //error handler
+      return res.status(200).send('<number>d<sides>')
+    }
+
+    //roll dice
+    for (var i = 0; i < times; i++){
+      var currentRoll = rol(1, die);
+      rolls.push(currentRoll);
+      total += currentRoll;
+    }
+    botPayload.username = 'dicebot';
+    botPayload.channel = req.body.channel_id;
+    botPayload.icon_emoji = ':game_die:'
+    botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' + rolls.join(' + ') + ' = *' + total + '*';
+
+    send(botPayload, function (error, status, body) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.status(200).end();
+      }
+    });
+  }
 }

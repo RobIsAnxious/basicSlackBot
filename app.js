1b470894c2f4d323620437e76af537d231310e5f
Rob Moore <rmoore.pageonepower@gmail.com>
2015-09-28 15:24:03 -0600

    fixed dicebot

--- app.js
+++ app.js
@@ -6,21 +6,26 @@ var dicebot = require('./dicebot');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// body parser middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// test route
-app.get('/', function (req, res) { res.status(200).send("Hello world!") });
+// testing purposes only
+app.get('/', function (req, res) { res.status(200).send('test') });
 
-// error handler
+// hellobot
+app.post('/hello', hellobot);
+
+// dicebot
+app.post('/roll', dicebot);
+
+
+// basic error handler
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
 });
 
+
 app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
 });
-
-app.post('/hello', hellobot);
-app.post('/roll', dicebot);
